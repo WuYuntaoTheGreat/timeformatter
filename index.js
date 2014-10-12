db006ecda498e06e72c75be77cfc77cfa73ec702
Wu Yuntao <wuyuntao@gmail.com>
2014-10-13 00:09:13 +0800

    Basic features

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * timeformater.js Copyright(c) 2014 Wu Yuntao
+ * https://github.com/WuYuntaoTheGreat/timeformatter
+ * Released under the MIT License.
+ */
+
+module.exports = require('./lib/timeformatter');
+
